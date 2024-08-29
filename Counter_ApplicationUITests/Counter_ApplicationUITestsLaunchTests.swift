efce7dba9618c21a0e059cbe16fc66598bbb6d2f
Saideep9835 <tsaideep98@gmail.com>
2024-08-29 10:39:33 -0500

    Initial Commit

---
+++ Counter_ApplicationUITests/Counter_ApplicationUITestsLaunchTests.swift
@@ -0,0 +1,32 @@
+//
+//  Counter_ApplicationUITestsLaunchTests.swift
+//  Counter_ApplicationUITests
+//
+//  Created by Saideep Reddy Talusani on 8/29/24.
+//
+
+import XCTest
+
+final class Counter_ApplicationUITestsLaunchTests: XCTestCase {
+
+    override class var runsForEachTargetApplicationUIConfiguration: Bool {
+        true
+    }
+
+    override func setUpWithError() throws {
+        continueAfterFailure = false
+    }
+
+    func testLaunch() throws {
+        let app = XCUIApplication()
+        app.launch()
+
+        // Insert steps here to perform after app launch but before taking a screenshot,
+        // such as logging into a test account or navigating somewhere in the app
+
+        let attachment = XCTAttachment(screenshot: app.screenshot())
+        attachment.name = "Launch Screen"
+        attachment.lifetime = .keepAlways
+        add(attachment)
+    }
+}
