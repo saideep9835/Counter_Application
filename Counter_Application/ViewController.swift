fbc2b9264ec03520d76381ab0b5a24d21fad98b8
Saideep9835 <tsaideep98@gmail.com>
2024-08-30 09:26:46 -0500

    Updated view controller with incrementing and decrementing counter.

--- Counter_Application/ViewController.swift
+++ Counter_Application/ViewController.swift
@@ -8,10 +8,29 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+   
+   
+   
+    @IBOutlet weak var countLabel: UILabel!
+    
+    var count: Int = 0
+    @IBAction func incrementCounter(_ sender: UIButton){
+      count += 1
+      updateCounter()
+}
+    @IBAction func decrementCounter(_ sender: UIButton) {
+        count -= 1
+        updateCounter()
+        
+    }
+    func updateCounter(){
+        countLabel.text = "Counter: \(String(describing: count))"
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        updateCounter()
     }
 
 
